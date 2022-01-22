098872eca034dfe422dfb69b30209e2fcd8fbc2b
pgates-test <Janick5234xz>
2022-01-22 16:46:59 +0100

    nowa metoda policz == update

--- main.cpp
+++ main.cpp
@@ -4,6 +4,6 @@ int Policz (int a, int b) {
 }
 int main() {
     std::cout << "Hello, World!" << std::endl;
-    std::cout <<"W pierwszym branchu; 2*3=" << Policz(2,3) << std::endl;
+    std::cout <<"W drugim branchu; 2*3=" << Policz(2,3) << std::endl;
     return 0;
 }
